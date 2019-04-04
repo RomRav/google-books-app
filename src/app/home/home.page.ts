be347f31b669193ee52a2738ecf94cdb6de4e012
Romain <romain.ravault@gmail.com>
2019-04-04 14:00:02 +0200

    Affichage et recherche books

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,62 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  public apiBooksKey: string = "AIzaSyAIKBVlFQxpdw6VAiAWR-K5zfETR8vlwQM";
+  public search: string;
+  public bookList: Array<any> = [];
+  public index: any = 0;
+  public choiceMaxResults: any = 20;
+
+  //Constructor qui a en paramétre une instanciation de la class HttpClient
+  constructor(private http: HttpClient) {
+
+
+    this.loadData(true, null, this.index);
+  }
+  /**
+   * Methode de récupération des livres
+   * @param addBeforeContent 
+   * @param even 
+   */
+  private loadData(addBeforeContent: boolean, even, index) {
+
+    let url = "https://www.googleapis.com/books/v1/volumes";
+    //Instanciation de la class HttpParams qui contient les paramétre a passer dans l'url
+    let requestParams = new HttpParams()
+      //Definition des paramétres (paramétre,valeur)
+      .set('q', this.search)
+      .set('key', this.apiBooksKey)
+      .set('maxResults', this.choiceMaxResults)
+      .set('startIndex', this.index);
+    console.log(this.index);
+    //la requet dans la variable req
+    let req = this.http.get(url, { params: requestParams });
+    //récupération de la réponse a la requet dans la variable data
+    req.subscribe((data: any) => {
+      console.log(data);
+      if (addBeforeContent) {
+        this.bookList = data.items.concat(this.bookList);
+      } else {
+        this.bookList = this.bookList.concat(data.items);
+      }
+      if (even) {
+        even.target.complet();
+      }
+      console.log(this.bookList);
+    })
+
+  }
+
+  private refreshData(even) {
+    this.index = this.bookList.length;
+    this.loadData(true, even, this.index);
+
+  }
+  private loadMoreData(even) {
+    this.index += this.bookList.length;
+    this.loadData(false, even, this.index);
+
+  }
 
 }
